bbbecc0db63c0f694818b54a7e540ad92b994d9c
Monroejack <Monroejack2121@gmail.com>
2018-04-19 18:29:15 -0400

    DRYer code

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,25 +1,19 @@
 require_relative '../calculator'
 
 describe 'Calculator' do
-  it 'adds two numbers' do
-    calc = Calculator.new(6, 3)
 
+  let(:calc) {Calculator.new(6,3)}
+  it 'adds two numbers' do
     expect(calc.addition).to eq(9)
   end
 
   it 'subtracts two numbers' do
-    calc = Calculator.new(6, 3)
-
     expect(calc.subtract).to eq(3)
   end
   it 'multiplies two numbers' do
-    calc = Calculator.new(6, 3)
-
     expect(calc.multiply).to eq(18)
   end
   it 'divides two numbers' do
-    calc = Calculator.new(6, 3)
-
     expect(calc.divide).to eq(2)
   end
 end
